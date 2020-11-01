ea294f6ecadad4cd6af3864e37ebc24f9fb7805a
Wellington Santos <weltossousa@gmail.com>
2020-11-01 10:53:30 -0300

    iniciando front end

---
+++ web/src/pages/Dashboard/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Dashboard: React.FC = () => {
+  return (
+    <>
+      <Container>
+        <h1>Hello world</h1>
+      </Container>
+    </>
+  );
+};
+
+export default Dashboard;
